9323d7aa8b59dae60a65b8319afe00b7b2375db7
Bahadir Biriktir <bahadirbrktr@gmail.com>
2023-03-09 22:48:36 +0100

    PHP

---
+++ php_variablen/index.php
@@ -0,0 +1,29 @@
+<?php
+
+$football = "Fussbalspiel";
+$diploma = "Matura";
+$price = "1.99€";
+$camelCase = "BahadirsDomain";
+$oatsmeal = "haferflocken";
+$game = "valorant";
+$water = "wasser";
+$brother = "bruder";
+$fittness = "fitte";
+$eggs = "eier";
+
+
+
+
+
+echo "ich habe heute einen $football<br>";
+echo "ich möchte nach den Schule $diploma machen<br>";
+echo "eine Banane kostet $price<br>";
+echo "ich habe ein Domain heisst $camelCase<br>";
+echo "$oatsmeal shake ist beste ding<br>";
+echo "ich spiele computergames am meisten $game<br>";
+echo "ich trinke jeden Tag 4 liter $water<br>";
+echo "mei $brother heisst Arthur<br>";
+echo "ich gehe 6 mal pro woche zum $fittness<br>";
+echo "beim Frühstück esse am meisten $eggs<br>";
+
+?>
